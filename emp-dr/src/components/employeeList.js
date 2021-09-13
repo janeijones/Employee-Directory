fa4c5394359332dd54dd4eef8d180dc02ffa7c33
Janei <18485105+janeijones@users.noreply.github.com>
2021-09-13 16:55:20 -0500

    Added columns for employees

--- emp-dr/src/components/employeeList.js
+++ emp-dr/src/components/employeeList.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-function employeelist (props) {
+function employeeList (props) {
     return (
         <table className = "table mt-4 table-striped">
             <thread>
                 <tr>
-                    
+                    <th scope = "col"> Name </th>
+                    <th scope = "col"> D.O.B. </th>
+                    <th scope = "col"> Phone Number </th>
+                    <th scope = "col"> Email </th>
                 </tr>
             </thread>
         </table>
