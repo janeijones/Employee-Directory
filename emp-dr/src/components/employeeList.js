3472f504bb414a2beae76aaf57493b4c41f374c7
Janei <18485105+janeijones@users.noreply.github.com>
2021-09-13 16:30:32 -0500

    return table wih employee detail columns

--- emp-dr/src/components/employeeList.js
+++ emp-dr/src/components/employeeList.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
-
+function employeelist (props) {
+    return (
+        <table className = "table mt-4 table-striped">
+            <thread>
+                <tr>
+                    
+                </tr>
+            </thread>
+        </table>
+    )
+}
 
 
 
