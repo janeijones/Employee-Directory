a1937c194c773cf385fc9eaeb081adea91c43cc9
Janei <18485105+janeijones@users.noreply.github.com>
2021-09-13 12:02:29 -0500

    Header rendering on page

--- emp-dr/src/components/Directory.js
+++ emp-dr/src/components/Directory.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import API from "../utils/API";
+import Header from "./Header"; 
 
 class Directory extends Component { // directory component defubed as class
     state = {
         search: "",
         results: []
-    }
 }; 
+
+
+
+
+  render() {
+    return <>
+    <Header />
+    </>
+  }
+}
