95fdc044f6c22c24d0e09a3ecde394bff5ef04a0
Janei <18485105+janeijones@users.noreply.github.com>
2021-09-13 10:45:57 -0500

    Added Directory component as a class

--- emp-dr/src/components/Directory.js
+++ emp-dr/src/components/Directory.js
@@ -1,2 +1,9 @@
 import React, { Component } from 'react';
 import API from "../utils/API";
+
+class Directory extends Component { // directory component defubed as class
+    state = {
+        search: "",
+        results: []
+    }
+}; 
