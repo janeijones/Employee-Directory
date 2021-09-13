22e108429c2f3e579f0e36a649097af62b3dc3eb
Janei <18485105+janeijones@users.noreply.github.com>
2021-09-13 10:26:51 -0500

    random user API query added from search method

--- emp-dr/src/utils/API.js
+++ emp-dr/src/utils/API.js
@@ -1,1 +1,9 @@
 import axios from "axios";
+
+const APIURL = "https://randomuser.me/api/?results=20&nat=us";
+
+export default { //export an object with a search method that searches the random user api for the passed query
+    search: function() {
+        return axios.get(APIURL);
+    }
+};
