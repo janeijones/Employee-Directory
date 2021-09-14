2faef607e3999ed54eda35fa205d36064c3cd517
Janei <18485105+janeijones@users.noreply.github.com>
2021-09-14 15:28:25 -0500

    employee info updated to include props

--- emp-dr/src/components/employeeInfo.js
+++ emp-dr/src/components/employeeInfo.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-function employeeInfo() {
-    return 
+function employeeInfo(props) {
+    return (
+        <tbody>
+            {props.resultsDisplay.map(results => (
+                console.log('here')
+                
+            ))}
+        </tbody>
+    )
        
 }
 
