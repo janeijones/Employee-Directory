218c43d4b15be911738c5545f453a546d5e4e1a4
Janei <18485105+janeijones@users.noreply.github.com>
2021-09-14 10:19:01 -0500

    userSearch added with form input

---
+++ emp-dr/src/components/userSearch.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function userSearch() {
+    return (
+        <form>
+            <input 
+                value = {props.userSearch}
+                onChange = {} //event here
+                placeholder = "Search"
+                />
+
+        </form>
+    )
+}
+
+export default userSearch; 
