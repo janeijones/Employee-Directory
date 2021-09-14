f1eb1afe4cff315964eaa6d1492bdceb29762874
Janei <18485105+janeijones@users.noreply.github.com>
2021-09-14 16:41:30 -0500

    Added dob and import for formatting date

--- emp-dr/src/components/employeeInfo.js
+++ emp-dr/src/components/employeeInfo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { format } from 'date-fns'
 
 function employeeInfo(props) {
     return (
@@ -8,6 +9,7 @@ function employeeInfo(props) {
                 <td>{results.name.first} {results.name.last} </td>
                 <td>{results.phone}</td>
                 <td>{results.email}</td>
+                <td>{format (new Date(results.dob.date), 'MM/dd/yyyy')}</td>
             
                 </tr>
                 
