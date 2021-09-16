79ce30fc274c40b762b0f9921e7b3ccee7167985
Janei <18485105+janeijones@users.noreply.github.com>
2021-09-16 02:16:12 -0500

    Added CSS styling

--- emp-dr/src/components/Header.js
+++ emp-dr/src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header (){
     return (
-        <div className = "container">
+        <div className = "container bg-light">
             <h1 className= "display-4 text-center"> Employee Directory </h1>
         </div>
     )
