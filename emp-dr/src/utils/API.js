0f936af370d951b2311d52814ad1433f4ec0e640
Janei <18485105+janeijones@users.noreply.github.com>
2021-09-17 06:10:21 -0500

    Deployment to ghpages

--- emp-dr/src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const APIURL = "https://randomuser.me/api/?results=20&nat=us";
-
-export default {
-    search: function() {
-      return axios.get(APIURL);
-    }
-  };
